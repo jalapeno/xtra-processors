1eb5ab5c383b2cd20fd4082c275bd2e74459c3cd
Bruce McDougall <brmcdoug@cisco.com>
2024-12-31 21:32:12 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ kubectl apply -f ipv4-graph.yaml
 kubectl apply -f ipv6-graph.yaml
 kubectl apply -f api-dep.yaml
 kubectl apply -f api-svc.yaml
-
+kubectl apply -f ui-dep.yaml
+kubectl apply -f ui-svc.yaml
