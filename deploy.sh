4c05c536ac7c5ddd1a90f184bf1a351bb5b8922a
Bruce McDougall <brmcdoug@cisco.com>
2025-02-17 18:25:23 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #/bin/sh
 
 kubectl apply -f igp-graph.yaml
-sleep 5
+echo "Waiting for 7 seconds for igp-graph to be ready"
+sleep 7
 kubectl apply -f ipv4-graph.yaml
 kubectl apply -f ipv6-graph.yaml
 kubectl apply -f api-dep.yaml
